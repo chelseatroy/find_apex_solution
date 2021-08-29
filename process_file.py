e561ef35dba60f54c53bc65ce1b4fcb465ab5b60
Chelsea Troy <chelsea.dommert@gmail.com>
2021-08-29 11:12:13 -0500

    Whoops! Missed a couple of integer conversions that could affect robustness of comparisons. Also added some comments for more clarity on what's happening.

--- process_file.py
+++ process_file.py
@@ -17,7 +17,7 @@ def process_file(filename, working_data_limit=80000):
             if batch: # At the end of a file, readlines will give []. bool([]) is falsy in Python.
                 if len(batch) == 1:
                     #Edge case: there's just one line in this batch
-                    if batch[0] > final_items_previous_batch[-1]:
+                    if int(batch[0]) > final_items_previous_batch[-1]:
                         raise ApexNotFoundException("Numbers only increase")
                     else:
                         # Did not extract this into a function, despite duplication on lines 32/33,
@@ -27,10 +27,13 @@ def process_file(filename, working_data_limit=80000):
                         item, local_index = find_apex_iterative(final_items_previous_batch + batch)
                         return item, index + local_index - len(final_items_previous_batch)
                 else:
-                    if int(batch[-1]) > int(batch[-2]):
+                    last_in_batch = int(batch[-1])
+                    second_last_in_batch = int(batch[-2])
+
+                    if last_in_batch > second_last_in_batch: # then integers are still ascending
                         index += (len(batch))
-                        final_items_previous_batch = batch[-2:]
+                        final_items_previous_batch = [second_last_in_batch, last_in_batch]
                         continue
-                    else:
+                    else: # integers have begun descending. Since we're evaluating batches sequentially, that means that this batch contains the apex.
                         item, local_index = find_apex_iterative(final_items_previous_batch + batch)
                         return item, index + local_index - len(final_items_previous_batch)
